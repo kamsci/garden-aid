71ba47badfbfecc1d6e4335618ed8e701618149c
SamaadTurner <samaad.turner@gmail.com>
2023-09-12 10:17:47 -0700

    finished Form  conversion to functional componennt

--- src/HelperComponents/FormModal.jsx
+++ src/HelperComponents/FormModal.jsx
@@ -1,40 +1,28 @@
-import { Component } from 'react';
-import { Button, Modal, Form } from 'react-bootstrap';
+import { Button, Modal, Form } from "react-bootstrap";
 
-class FormModal extends Component {
-
-    render() {
-        const { 
-            title,
-            show, 
-            handleClose, 
-            handleShow, 
-            onSubmit,
-            children 
-        } = this.props;
-        return (
-            <>
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header>
-                <Modal.Title>{title}</Modal.Title>
-                </Modal.Header>
-                <Form onSubmit={onSubmit}>
-                    <Modal.Body>
-                        {children}
-                    </Modal.Body>
-                    <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close without saving
-                    </Button>
-                    <Button variant="primary" type="submit">
-                        Save
-                    </Button>
-                    </Modal.Footer>
-                </Form>
-            </Modal>
-            </>
-        );
-    }
+const FormModal = (props) => {
+    const {title, show, handleClose, onSubmit, children} = props;
+    return (
+        <>
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header>
+            <Modal.Title>{title}</Modal.Title>
+            </Modal.Header>
+            <Form onSubmit={onSubmit}>
+                <Modal.Body>
+                    {children}
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close without saving
+                </Button>
+                <Button variant="primary" type="submit">
+                    Save
+                </Button>
+                </Modal.Footer>
+            </Form>
+        </Modal>
+        </>
+    );
 }
-
 export default FormModal;
